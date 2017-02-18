0f2c858e9088bc9719201d321bfbaf445e92659f
Riza Fahmi <rizafahmi@gmail.com>
2017-02-18 16:48:37 +0700

    Finish DELETE

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,8 +1,53 @@
-const TodoController = (req, res, next) => {
+const index = (req, res, next) => {
   req.app.get('db').todo.find((err, result) => {
-    if (err) return next(err)
+    if (err) {
+      return next(err)
+    }
     res.json(result)
   })
 }
 
-module.exports = TodoController
+const create = (req, res, next) => {
+  req.app.get('db').todo.save({
+    text: req.body.text,
+    createdAt: new Date()
+  }, (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.json(result)
+  })
+}
+
+const put = (req, res, next) => {
+  req.app.get('db').todo.save({
+    id: req.params.id,
+    text: req.body.text
+  }, (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.json(result)
+  })
+}
+
+const remove = (req, res, next) => {
+  req.app.get('db').todo.destroy({
+    id: req.params.id
+  }, (err, result) => {
+    debugger
+    if (err) {
+      return next(err)
+    } else if (!result) {
+      return res.status(404).send('not found')
+    }
+    res.json(result)
+  })
+}
+
+module.exports = {
+  index,
+  create,
+  put,
+  remove
+}
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const TodoController = require('../controllers/todo_controller.js')
 
 
-/* GET home page. */
-router.get('/', TodoController)
+router.get('/', TodoController.index)
+router.post('/', TodoController.create)
+router.put('/:id', TodoController.put)
+router.delete('/:id', TodoController.remove)
 
 module.exports = router
